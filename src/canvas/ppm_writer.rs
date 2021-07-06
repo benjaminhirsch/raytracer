2c6abf46703cda7fd8c835bf65044544279925bf
Benjamin Hirsch <mail@benjaminhirsch.net>
2021-07-06 22:12:08 +0200

    Clean up

--- src/canvas/ppm_writer.rs
+++ src/canvas/ppm_writer.rs
@@ -2,11 +2,11 @@ use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
 use crate::canvas::canvas::Canvas;
-use crate::color::color::Color;
 use std::fs::{create_dir, File};
 use std::io::{Error, Write};
 use std::path::Path;
 
+#[allow(dead_code, unused_must_use)]
 pub fn write_ppm(canvas: Canvas) -> Result<Box<String>, Error> {
     let mut rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #[macro_use]
 extern crate approx;
 
-use crate::canvas::canvas::Canvas;
-use crate::canvas::ppm_writer::write_ppm;
-use crate::color::color::Color;
-
 mod canvas;
 mod color;
 mod math;
